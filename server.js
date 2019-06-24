b428d38193f2e3ada1ea671f458a957472e937fa
Lalit Thapa <lalit.thapa@ekbana.info>
2019-06-24 11:48:17 +0545

    Change env variables to string

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const Pusher = require('pusher')
 const axios = require('axios')
 
 // Initialise Pusher
-var pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID,
-  key: process.env.PUSHER_APP_KEY,
-  secret: process.env.PUSHER_APP_SECRET,
-  cluster: process.env.PUSHER_APP_CLUSTER,
-  encrypted: true,
+const pusher = new Pusher({
+  appId: `${process.env.PUSHER_APP_ID}`,
+  key: `${process.env.PUSHER_APP_KEY}`,
+  secret: `${process.env.PUSHER_APP_SECRET}`,
+  cluster: `${process.env.PUSHER_APP_CLUSTER}`,
+  userTLS: true,
 })
 
 // Body parser middleware
