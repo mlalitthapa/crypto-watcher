2ce7624789583dd2f3755fa15a197936570b791e
Lalit Thapa <lalit.thapa@ekbana.info>
2019-06-24 11:24:16 +0545

    Import and use axios and moment plugins

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import moment from 'moment';
+import VueMomentJS from 'vue-momentjs';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+Vue.use(VueMomentJS, moment);
 
 Vue.config.productionTip = false
 
