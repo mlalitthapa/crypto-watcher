7899ee8096a559e4cc5238ccc76413d6ff1ccdc6
Lalit Thapa <lalit.thapa@ekbana.info>
2019-06-24 13:42:47 +0545

    Config webpack-dotent

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,6 +4,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const Dotenv = require('dotenv-webpack')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -13,6 +14,9 @@ module.exports = {
   entry: {
     app: './src/main.js'
   },
+  plugins: [
+    new Dotenv()
+  ],
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
